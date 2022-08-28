71ab36516ee11bc56c3cec7a5af80247b1315eaf
Mau Abata <mauabata@gmail.com>
2022-08-28 00:12:57 -0500

    Now with 110% more CSS

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+// CSS
+import 'scss/index.scss';
+
+import {AppProps} from 'next/app'
+
+export default function App({Component, pageProps}: AppProps): JSX.Element {
+    return <Component {...pageProps} />
+}
