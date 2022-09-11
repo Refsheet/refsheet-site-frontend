f100b032e994df7ad59fc5b9a065151c70174592
Mau Abata <mauabata@gmail.com>
2022-09-10 19:00:58 -0500

    This is, once again, becoming an entire rebuild of Refsheet.

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -4,6 +4,10 @@ import {ParsedUrlQuery} from "querystring";
 import client from "services/ApplicationService";
 import getUserProfile from './getUserProfile.graphql';
 import NotFound from "components/Shared/views/NotFound";
+import Main from "../../src/components/Shared/Main";
+import Header from "../../src/v1/views/user/Header";
+import Characters from "../../src/v1/views/user/Characters";
+import Section from 'components/Shared/Section';
 
 export interface UserProfileParams extends ParsedUrlQuery {
     username: string;
@@ -19,11 +23,21 @@ export interface IUserProfile {
 
 const UserProfile: React.FC<UserProfileProps> = ({user}) => {
     if (!user) return <NotFound/>
-    
+
     return (
-        <div>
-            <h1>{user.name}</h1>
-        </div>
+        <Main title={[user.name, 'Users']}>
+            <Header
+                user={user}
+                blocked={user.is_blocked}
+                blocks={user.blocks}
+                followed={user.is_followed}
+                onFollow={() => null}
+            />
+
+            <Section container className="margin-top--large padding-bottom--none">
+                ( characters )
+            </Section>
+        </Main>
     );
 }
 
