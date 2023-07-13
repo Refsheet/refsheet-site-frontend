eb245fc9bfa2a2496c967bcb5c3f101ed81145da
Alec Deitloff <alec.deitloff@gmail.com>
2023-07-12 20:30:54 -0700

    Produce a 404 from server-side when querying nonexistent user profile

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,34 +1,33 @@
 import React from 'react'
-import {GetServerSideProps} from "next";
-import {ParsedUrlQuery} from "querystring";
 import client from "services/ApplicationService";
 import getUserProfile from '../../src/graph/queries/getUserProfile.graphql';
-import NotFound from "components/Shared/views/NotFound";
 import UserView from 'components/User/View';
 import {GetUserProfileQuery} from "../../@types/schema";
-
-export interface UserProfileParams extends ParsedUrlQuery {
-    username: string;
-}
+import type { GsspParams, GsspResult } from '@refsheet/types';
 
 export interface UserProfileProps {
-    user: GetUserProfileQuery['getUser'];
+    user: NonNullable<GetUserProfileQuery['getUser']>;
 }
 
 const UserProfile: React.FC<UserProfileProps> = ({user}) => {
-    if (!user) return <NotFound/>;
     return <UserView user={user}/>;
 }
 
-export const getServerSideProps: GetServerSideProps<UserProfileProps, UserProfileParams> = async ({params}) => {
+export async function getServerSideProps({params}: GsspParams<{ username: string }>): GsspResult<UserProfileProps> {
     const {data} = await client.query<GetUserProfileQuery>({
         query: getUserProfile,
         variables: {username: params?.username}
     });
 
+    if (!data.getUser) {
+        return {
+            notFound: true,
+        }
+    }
+
     return {
         props: {
-            user: data.getUser || null
+            user: data.getUser
         }
     }
 }
